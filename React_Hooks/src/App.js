2e0373c338f2fb0d30251ca379297a2f65bdd33b
Tolegithub7 <Tolekadima7@gmail.com>
2024-03-09 16:06:41 +0300

    coded useState

--- React_Hooks/src/App.js
+++ React_Hooks/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-
+import CounterUsingState from "./Components/UseState/CounterUsingState";
+import CounterIncrement from "./Components/UseState/CounterIncrement";
 function App() {
   return (
     <div className="App">
       <h3>React Hooks</h3>
+      <CounterUsingState />
+      <CounterIncrement />
     </div>
   );
 }
--- React_Hooks/src/Components/UseState/CounterIncrement.js
+++ React_Hooks/src/Components/UseState/CounterIncrement.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Counter.css";
 
 function CounterIncrement() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h3>{count}</h3>
+      <button
+        type="button"
+        className="RiseUp"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        RiseUP
+      </button>
+      <button
+        type="button"
+        className="GetDown"
+        onClick={() => setCount((count) => count - 1)}
+      >
+        GetDown
+      </button>
+      <button type="button" className="Reset" onClick={() => setCount(0)}>
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default CounterIncrement;
