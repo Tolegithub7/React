b8f2b776f6e241df75c4fef4859f69f0ecba3b1a
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-30 02:58:17 +0300

    Navbar

--- Ebusers/src/Components/Navbar/Navbar.jsx
+++ Ebusers/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import logo from "../../assets/file.png";
 const Navbar = () => {
   return (
-    <div>
+    <nav>
       <img src={logo} alt="" />
-    </div>
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About Us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
   );
 };
 
