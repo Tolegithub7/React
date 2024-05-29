027f42ffb5b01147f47cea5ccdecaaff3b92e802
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-30 01:58:07 +0300

    fucked

--- Ebusers/src/App.jsx
+++ Ebusers/src/App.jsx
@@ -1,9 +1,11 @@
-// import React from "react";
+import React from "react";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 const App = () => {
   return (
     <div>
       <h1>Hello, Ebusers!</h1>
+      <Navbar />
     </div>
   );
 };
