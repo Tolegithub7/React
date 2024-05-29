591851c9b89dad6ca481c53d1952b9680cbfa273
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-30 02:36:10 +0300

    going well right

--- Ebusers/src/App.jsx
+++ Ebusers/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
-import file from "./assets/file.png";
-import logo from "./assets/Logo (2).png";
+import Navbar from "./Components/Navbar/Navbar";
+// import file from "./assets/file.png";
+// import logo from "./assets/Logo (2).png";
 
 function App() {
   return (
     <>
-      <img src={file} alt="" />
+      {/* <img src={file} alt="" /> */}
       <h1>Ebusers</h1>
-      <img src={logo} alt="" />
+      <Navbar />
+      {/* <img src={logo} alt="" /> */}
     </>
   );
 }
---
+++ Ebusers/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import logo from "../../assets/file.png";
+const Navbar = () => {
+  return (
+    <div>
+      <img src={logo} alt="" />
+    </div>
+  );
+};
+
+export default Navbar;
