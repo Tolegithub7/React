450f0d9143ac6cadf81b3e217379c4ec4445ac95
Tolegithub7 <Tolekadima7@gmail.com>
2024-05-30 16:06:25 +0300

    working on hero css 31

--- Ebusers/src/Components/Hero/Hero.jsx
+++ Ebusers/src/Components/Hero/Hero.jsx
@@ -1,7 +1,21 @@
 import "./Hero.css";
 
 const Hero = () => {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>
+          Education is the key to unlocking the world, a passport to freedom.
+        </h1>
+        <p>
+          Our cutting edge curriculum is designed to empower students with the
+          knowledge, skill and experience needed to excel in the dynamic field
+          of education
+        </p>
+        <button className="btn">Explore More →</button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
