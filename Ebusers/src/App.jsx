27e58a38ff942caeae16178db2156e003b8d6805
Tolegithub7 <Tolekadima7@gmail.com>
2024-06-03 22:35:11 +0300

    Create new folder Title

--- Ebusers/src/App.jsx
+++ Ebusers/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
+import Title from "./Components/Title/Title";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Title />
       <div className="container">
         <Programs />
       </div>
