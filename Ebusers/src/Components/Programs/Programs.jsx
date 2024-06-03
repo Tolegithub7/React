401aaa1240cbe36107a3f5fd99d57d7e11b1191f
Tolegithub7 <Tolekadima7@gmail.com>
2024-06-03 21:51:29 +0300

    adding captionn and icons

--- Ebusers/src/Components/Programs/Programs.jsx
+++ Ebusers/src/Components/Programs/Programs.jsx
@@ -2,6 +2,9 @@ import "./Programs.css";
 import program1 from "../../assets/program1.jpg";
 import program2 from "../../assets/program2.jpg";
 import program3 from "../../assets/program3.jpg";
+import Program1_icon from "../../assets/program-icon-1.png";
+import Program2_icon from "../../assets/program-icon-2.png";
+import Program3_icon from "../../assets/program-icon-3.png";
 
 const Programs = () => {
   return (
@@ -9,15 +12,23 @@ const Programs = () => {
       <div className="program">
         <img src={program1} alt="" />
         <div className="caption">
-          <img src="" alt="" />
+          <img src={Program1_icon} alt="" />
           <p>Graduation degree</p>
         </div>
       </div>
       <div className="program">
         <img src={program2} alt="" />
+        <div className="caption">
+          <img src={Program2_icon} alt="" />
+          <p>Masters degree</p>
+        </div>
       </div>
       <div className="program">
         <img src={program3} alt="" />
+        <div className="caption">
+          <img src={Program3_icon} alt="" />
+          <p>PHD Program</p>
+        </div>
       </div>
     </div>
   );
