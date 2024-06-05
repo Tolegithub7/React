52b4e5be86b19ab887785e85d46b6481181dceb2
Tolegithub7 <Tolekadima7@gmail.com>
2024-06-05 20:55:27 +0300

    add useState and useEffect for scroll navbar change

--- Ebusers/src/Components/Navbar/Navbar.jsx
+++ Ebusers/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
 import logo from "../../assets/file (1).png";
 import "./Navbar.css";
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
+
   return (
-    <nav className="container">
+    <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
         <li>Home</li>
