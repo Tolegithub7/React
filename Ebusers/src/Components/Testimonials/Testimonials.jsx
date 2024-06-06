cd45d2333134ce754e234d701c6fef514c542870
Tolegithub7 <Tolekadima7@gmail.com>
2024-06-06 11:00:26 +0300

    will uncomment letter

--- Ebusers/src/Components/Testimonials/Testimonials.jsx
+++ Ebusers/src/Components/Testimonials/Testimonials.jsx
@@ -1,10 +1,10 @@
 import "./Testimonials.css";
 import nexticon from "../../assets/next-icon.png";
 import backicon from "../../assets/back-icon.png";
-import user1 from "../../assets/user-1.jpeg";
-import user2 from "../../assets/user-2.jpg";
-import user3 from "../../assets/user-3.jpg";
-import user4 from "../../assets/user-4.jpeg";
+// import user1 from "../../assets/user-1.jpeg";
+// import user2 from "../../assets/user-2.jpg";
+// import user3 from "../../assets/user-3.jpg";
+// import user4 from "../../assets/user-4.jpeg";
 
 const Testimonials = () => {
   return (
