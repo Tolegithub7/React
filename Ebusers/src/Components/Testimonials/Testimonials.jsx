0cdb20b3ef05be74e760c07295ebbc8a3ce38aee
Tolegithub7 <Tolekadima7@gmail.com>
2024-06-06 10:56:44 +0300

    next and back icon in testimonials

--- Ebusers/src/Components/Testimonials/Testimonials.jsx
+++ Ebusers/src/Components/Testimonials/Testimonials.jsx
@@ -1,7 +1,18 @@
 import "./Testimonials.css";
+import nexticon from "../../assets/next-icon.png";
+import backicon from "../../assets/back-icon.png";
+import user1 from "../../assets/user-1.jpeg";
+import user2 from "../../assets/user-2.jpg";
+import user3 from "../../assets/user-3.jpg";
+import user4 from "../../assets/user-4.jpeg";
 
 const Testimonials = () => {
-  return <div></div>;
+  return (
+    <div>
+      <img src={nexticon} alt="" className="next-btn" />
+      <img src={backicon} alt="" className="back-btn" />
+    </div>
+  );
 };
 
 export default Testimonials;
