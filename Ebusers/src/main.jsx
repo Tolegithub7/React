4ea1e0a4975e0796e83c3d5b8a48f2e2abf14988
Tolegithub7 <Tolekadima7@gmail.com>
2024-07-18 23:00:35 +0300

    update main.jsx

--- Ebusers/src/main.jsx
+++ Ebusers/src/main.jsx
@@ -1,27 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import App from "./App.jsx";
+// import "./index.css";
+
+// // ReactDOM.createRoot(document.getElementById("root")).render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
+// // );
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -29,3 +27,16 @@ ReactDOM.render(
 //   </React.StrictMode>,
 //   document.getElementById("root")
 // );
+// // const root = ReactDOM.createRoot(document.getElementById("root"));
+// // root.render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
+// // );
+
+// // ReactDOM.render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>,
+// //   document.getElementById("root")
+// // );
