8695d6597a3327f1f0cabb9b9be8841a5c93f765
Tolegithub7 <Tolekadima7@gmail.com>
2024-08-02 22:06:48 +0300

    updates

--- Ebusers/src/Components/Testimonials/Testimonials.jsx
+++ Ebusers/src/Components/Testimonials/Testimonials.jsx
@@ -1,22 +1,87 @@
 import "./Testimonials.css";
 import nexticon from "../../assets/next-icon.png";
 import backicon from "../../assets/back-icon.png";
-// import user1 from "../../assets/user-1.jpeg";
-// import user2 from "../../assets/user-2.jpg";
-// import user3 from "../../assets/user-3.jpg";
-// import user4 from "../../assets/user-4.jpeg";
+import user1 from "../../assets/user-1.jpeg";
+import user2 from "../../assets/user-2.jpg";
+import user3 from "../../assets/user-3.jpg";
+import user4 from "../../assets/user-4.jpeg";
 
 const Testimonials = () => {
   return (
     <div className="testimonials">
       <img src={nexticon} alt="" className="next-btn" />
       <img src={backicon} alt="" className="back-btn" />
-      {/* <img src={user1} alt="" />
-      <img src={user2} alt="" />
-      <img src={user3} alt="" />
-      <img src={user4} alt="" /> */}
       <div className="slider">
-        <p>uuuuuuuuuuuuuuuuuuu</p>
+        <ul>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user1} alt="" />
+                <div>
+                  <h3>Duresa Guye</h3>
+                  <span>Ebusers, Ethiopia</span>
+                </div>
+              </div>
+              <p>
+                Choosing to pursue my degree at Ebusers was one of the best
+                decisions I&#39;ve ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have truly exceeded my expectations.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user2} alt="" />
+                <div>
+                  <h3>Tilahun Diriba</h3>
+                  <span>Ebusers, Ethiopia</span>
+                </div>
+              </div>
+              <p>
+                Choose to pursue my degree at Ebusers was one of the best
+                decisions I&#39;ve ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have truly exceeded my expectations.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user3} alt="" />
+                <div>
+                  <h3>Faysal Seifu</h3>
+                  <span>Ebusers, Ethiopia</span>
+                </div>
+              </div>
+              <p>
+                Choose to pursue my degree at Ebusers was one of the best
+                decisions I&#39;ve ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have truly exceeded my expectations.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user4} alt="" />
+                <div>
+                  <h3>Alpha Lencho</h3>
+                  <span>Ebusers, Ethiopia</span>
+                </div>
+              </div>
+              <p>
+                Choose to pursue my degree at Ebusers was one of the best
+                decisions I&#39;ve ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have truly exceeded my expectations.
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
