fcce6fea7e8d9939ebf16d95663f0d4ddecb2b0d
Tolegithub7 <Tolekadima7@gmail.com>
2024-08-03 15:15:41 +0300

    form complete with web3forms.com

--- Ebusers/src/Components/Contact/Contact.jsx
+++ Ebusers/src/Components/Contact/Contact.jsx
@@ -1,7 +1,33 @@
+import React from "react";
 import "./Contact.css";
 import msg_icon from "../../assets/msg-icon.png";
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "ecbef21c-800a-4807-aad2-d29e7689662d");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div className="contact">
       <div className="contact-col">
@@ -31,7 +57,37 @@ const Contact = () => {
           </li>
         </ul>
       </div>
-      <div className="contact-col"></div>
+      {/* I want vertical line here */}
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label htmlFor="name">Your Name: </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+          <label htmlFor="phone">Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter your mobile phone number"
+            required
+          />
+          <label htmlFor="messaage">Write Your Message Here</label>
+          <textarea
+            name="message"
+            rows="6"
+            id=""
+            placeholder="Enter your message here"
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            {" "}
+            Submit now
+          </button>
+        </form>
+        <span>{result}</span>
+      </div>
     </div>
   );
 };
