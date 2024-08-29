2217d6f35ec01ca5236cea7f96aa7720fbc214fa
Tolegithub7 <Tolekadima7@gmail.com>
2024-08-29 21:22:43 +0300

    add button for desktop while logged in

--- promtopia/components/Nav.jsx
+++ promtopia/components/Nav.jsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from "react";
 import { SignIn, SignOut, UseSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-6 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -18,6 +20,19 @@ const Nav = () => {
         />
         <p className="logo_text">Promtopia</p>
       </Link>
+      {/* Desktop */}
+      <div className="sm:flex hidden">
+        {/* wc button to show whether user logged in or not */}
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
