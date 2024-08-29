a0e79f6b5d690dbf577fef1580c6ed5a27116b37
Tolegithub7 <Tolekadima7@gmail.com>
2024-08-29 10:06:39 +0300

    modification and logo added

--- promtopia/components/Feed.jsx
+++ promtopia/components/Feed.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Feed = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Feed</h1>
+    </div>
+  );
 };
 
 export default Feed;
---
+++ promtopia/components/Nav.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { SignIn, SignOut, UseSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+  return (
+    <nav className="flex-between w-full mb-6 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="assets/images/logo.svg"
+          className="object-contain"
+          width={30}
+          height={30}
+          alt="Promptopia logo"
+        />
+      </Link>
+    </nav>
+  );
+};
+
+export default Nav;
